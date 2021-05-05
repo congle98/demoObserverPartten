f4b6c26087e418627742266d999bde0cd167fbc6
congle98 <79907265+congle98@users.noreply.github.com>
2021-05-05 13:38:55 +0700

    addfile cách sử dụng ObserverPartten

---
+++ src/views/Client.java
@@ -0,0 +1,23 @@
+package views;
+
+import model.BinObserver;
+import model.HexObserver;
+import model.OctObserver;
+import model.Subject;
+
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        Subject sub = new Subject();
+        // Client configures the number and type of Observers
+        new HexObserver(sub);
+        new OctObserver(sub);
+        new BinObserver(sub);
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.print("\nEnter a number: ");
+            sub.setState(scan.nextInt());
+        }
+    }
+}
